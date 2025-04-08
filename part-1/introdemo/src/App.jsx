5c53b869c3b2b4bc76e01002eb1acc08ef458413
Sindhu SR <sindhusr@Sindhus-MacBook-Pro.local>
2025-04-08 11:26:16 +0530

    unicafe ex-1.1 step6

--- part-1/introdemo/src/App.jsx
+++ part-1/introdemo/src/App.jsx
@@ -9,6 +9,20 @@ const StatisticsLine = ({ text, value }) => {
   )
 }
 
+const StatisticsTable = ({ good, neutral, bad }) => {
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={good + neutral + bad} />    
+        <StatisticsLine text="average" value={(good - bad) / (good + neutral + bad)} />
+        <StatisticsLine text="positive" value={`${(good / (good + neutral + bad)) * 100} %`} />
+      </tbody>
+    </table>
+  )
+}
 const Statistics = ({ good, neutral, bad }) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return (
@@ -21,12 +35,7 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={good + neutral + bad} />
-      <StatisticsLine text="average" value={(good - bad) / (good + neutral + bad)} />
-      <StatisticsLine text="positive" value={`${(good / (good + neutral + bad)) * 100} %`} />
+      <StatisticsTable good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
