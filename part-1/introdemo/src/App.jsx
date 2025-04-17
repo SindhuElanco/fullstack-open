013d84a38d174e5932e2ee8dbdf37a097177a1ed
Sindhu SR <sindhusr@Sindhus-MacBook-Pro.local>
2025-04-17 08:46:38 +0530

    anecdote step 2 and 3

--- part-1/introdemo/src/App.jsx
+++ part-1/introdemo/src/App.jsx
@@ -12,16 +12,39 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
+  const arr = new Array(anecdotes.length).fill(0)
+
   const [selected, setSelected] = useState(0)
   console.log('selected', selected)
 
+  const [votesState, setVotesState] = useState(arr)
+
+  const handleVote = () => {
+    const copy = [...votesState]
+    copy[selected] += 1
+    setVotesState(copy)
+    console.log('votesState', votesState)
+  }
+  //votestate = [1,3,0,5,2]
+  // votestateindex = [0,1,2,3,4]
+  const max = Math.max(...votesState) //has the max in the votestate array
+  const indexanecdote = votesState.indexOf(max) //gets the index of the max value of the voteState array
+  console.log('max', votesState.indexOf(max))
+
   return (
     <div>
-      {anecdotes[selected]}
+      {anecdotes[selected]} <br/> has {votesState[selected]} votes
       <br/>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
         next anecdote
       </button>
+      <button onClick={handleVote}>Vote</button>
+      <br/>
+      <h2>Anecdote with most votes</h2>
+      <p>{max == 0 ? '' : anecdotes[indexanecdote]} </p> 
+      {/* if the value of max is 0 that is when the page loads or when no anecdotes is voted, the anecdotes with most votes should be blank */}
+
+
     </div>
   )
 }
