6e25ce5455f241c66df56ef9e4948a9f48e75100
Sindhu SR <sindhuelanco@gmail.com>
2025-06-11 21:26:32 +0530

    phonebook step 1

--- part-1/introdemo/src/App.jsx
+++ part-1/introdemo/src/App.jsx
@@ -6,20 +6,27 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+  
   return (
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((p) => <p key={p.name}>{p.name}</p>)}
+      
     </div>
   )
 }
