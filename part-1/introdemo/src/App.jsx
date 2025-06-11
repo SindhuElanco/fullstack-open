ecaa891e7c816eab4843a0b405ce284396652eb1
Sindhu SR <sindhuelanco@gmail.com>
2025-06-11 21:39:58 +0530

    phonebook: ex-2.7 step 2

--- part-1/introdemo/src/App.jsx
+++ part-1/introdemo/src/App.jsx
@@ -8,6 +8,13 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    const names = persons.map((p) => p.name)
+   
+    if(names.includes(newName)){
+      window.alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
     setPersons([...persons, { name: newName }])
     setNewName('')
   }
